3505f521e08f157e5ae8c87dc2ebb444867e37fd
IGWE PRECIOUS IFECHUKWU <igwep5537@gmail.com>
2026-01-04 14:50:25 +0100

    update: added swagger documentation

---
+++ src/docs/swagger.ts
@@ -0,0 +1,28 @@
+import swaggerJsdoc from "swagger-jsdoc";
+
+export const swaggerSpec = swaggerJsdoc({
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "CRUD Backend API",
+      version: "1.0.0",
+      description: "User Management API with Auth & RBAC",
+    },
+    servers: [
+      {
+       url: process.env.BASE_URL || "http://localhost:5000",
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    security: [{ bearerAuth: [] }],
+  },
+  apis: ["./src/routes/**/*.ts"],
+});
